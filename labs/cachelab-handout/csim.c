6e2c231aec307b88bd09d4430fa0e40a5060a0b6
Chenrui Wang <ChenruiStats@outlook.com>
2024-02-07 19:07:53 +0800

    Add usage and arguments' definitions

--- labs/cachelab-handout/csim.c
+++ labs/cachelab-handout/csim.c
@@ -1,7 +1,28 @@
 #include "cachelab.h"
 
+/*
+############  csim usage ##############
+
+Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>
+Options:
+  -h         Print this help message.
+  -v         Optional verbose flag.
+  -s <num>   Number of set index bits.
+  -E <num>   Number of lines per set.
+  -b <num>   Number of block offset bits.
+  -t <file>  Trace file.
+
+Examples:
+  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace
+  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace
+*/
+
 int main()
 {
+    int ch;
+    int set_index_bits, lines_per_set, block_offset_bits;
+    char* trace_file;
+
     printSummary(0, 0, 0);
     return 0;
 }
