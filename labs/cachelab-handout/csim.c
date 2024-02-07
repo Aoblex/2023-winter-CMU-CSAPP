40f2db3e31faf169d3d4983480ce165240dcc3d9
Chenrui Wang <ChenruiStats@outlook.com>
2024-02-07 19:17:24 +0800

    Add switch cases

--- labs/cachelab-handout/csim.c
+++ labs/cachelab-handout/csim.c
@@ -21,11 +21,30 @@ Examples:
   linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace
 */
 
-int main()
-{
+int main(int argc, char* argv[]){
     int ch;
     int set_index_bits, lines_per_set, block_offset_bits;
     char* trace_file;
+    const char* optstring = "hvs:E:b:t:";
+
+    while(~(ch = getopt(argc, argv, optstring))){
+        switch(ch){
+            case 'h':
+                break;
+            case 'v':
+                break;
+            case 's':
+                break;
+            case 'E':
+                break;
+            case 'b':
+                break;
+            case 't':
+                break;
+            default:
+                break;
+        }
+    }
 
     printSummary(0, 0, 0);
     return 0;
