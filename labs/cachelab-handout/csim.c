2a260f273cba53ce3326aa2b3c8e3f6295712c4a
Chenrui Wang <ChenruiStats@outlook.com>
2024-02-10 14:00:42 +0800

    Reconstruct code

--- labs/cachelab-handout/csim.c
+++ labs/cachelab-handout/csim.c
@@ -52,7 +52,7 @@ typedef struct
     int length;
 } CacheSetHeader;
 
-// max number of trace entries
+// Max number of trace entries
 #define ENTRIES 65536
 
 // Count results
@@ -64,58 +64,23 @@ unsigned long long tag_mask, set_mask, block_mask;
 unsigned int num_sets = 0;
 CacheSetHeader *cache_set_headers;
 
-// optional flags
+// Optional flags
 bool help_flag = false;
 bool verbose_flag = false;
 bool invalid_flag = false;
 const char *optstring = "hvs:E:b:t:";
 
-// file parameters
+// File parameters
 char *trace_file_path;
 FILE *trace_file = NULL;
 Trace trace_entries[ENTRIES];
 int entries_count = 0;
-
-void record_result(Trace *trace_entry, char result_type)
-{
-    trace_entry->operation_results[trace_entry->result_count] = result_type;
-    trace_entry->result_count += 1;
-    switch (result_type)
-    {
-    case 'h':
-        hit_count += 1;
-        break;
-    case 'e':
-        eviction_count += 1;
-        break;
-    case 'm':
-        miss_count += 1;
-        break;
-    default:
-        printf("Invalid result type: %c\n", result_type);
-        exit(EXIT_FAILURE);
-        break;
-    }
-}
 
 void execute_data_load(CacheSetHeader *cache_set_headers, Trace *trace_entry)
 {
     unsigned long long set_index = trace_entry->index.set_index;
     unsigned long long tag_index = trace_entry->index.tag_index;
     CacheSetHeader cache_set_header = cache_set_headers[set_index];
-    CacheLine *current_line_ptr = cache_set_header.head;
-    while (current_line_ptr != NULL)
-    {
-        if (current_line_ptr->tag_index == tag_index)
-        {
-            record_result(trace_entry, 'h');
-            return;
-        }
-        current_line_ptr = current_line_ptr->next;
-    }
-
-    // no hit, then record a miss
-    record_result(trace_entry, 'm');
 }
 
 void execute_data_store(CacheSetHeader *cache_set_headers, Trace *trace_entry)
@@ -139,14 +104,78 @@ void execute_command(CacheSetHeader *cache_set_headers, Trace *trace_entry)
         execute_data_store(cache_set_headers, trace_entry);
         break;
     case 'I':
-        break; // do nothing.
+        break; // Do nothing
     default:
         printf("Invalid operation: '%c'.\n", operation);
         exit(EXIT_FAILURE);
     }
 }
 
-int main(int argc, char *argv[])
+void record_result(Trace *trace_entry, char result_type)
+{
+    trace_entry->operation_results[trace_entry->result_count] = result_type;
+    trace_entry->result_count += 1;
+    switch (result_type)
+    {
+    case 'h':
+        hit_count += 1;
+        break;
+    case 'e':
+        eviction_count += 1;
+        break;
+    case 'm':
+        miss_count += 1;
+        break;
+    default:
+        printf("Invalid result type: %c\n", result_type);
+        exit(EXIT_FAILURE);
+        break;
+    }
+}
+
+void read_traces()
+{
+    while (~(fscanf(trace_file, "%s %llx,%hd \n", trace_entries[entries_count].operation, &trace_entries[entries_count].address, &trace_entries[entries_count].size)))
+    {
+        unsigned long long current_address = trace_entries[entries_count].address;
+        trace_entries[entries_count].index.tag_index = (tag_mask & current_address) >> (set_index_bits + block_offset_bits);
+        trace_entries[entries_count].index.set_index = (set_mask & current_address) >> block_offset_bits;
+        trace_entries[entries_count].index.block_index = block_mask & current_address;
+        trace_entries[entries_count].result_count = 0;
+        for (int i = 0; i < 4; ++i)
+        {
+            trace_entries[entries_count].operation_results[i] = '\0';
+        }
+        ++entries_count;
+    }
+    fclose(trace_file);
+}
+
+void initialize_cache()
+{
+
+    // Generate masks
+    block_mask = (1ull << block_offset_bits) - 1;
+    set_mask = ((1ull << set_index_bits) - 1) << block_offset_bits;
+    tag_mask = (ULLONG_MAX ^ set_mask) ^ block_mask;
+    num_sets = 1u << set_index_bits;
+
+    // Allocate spaces for cache
+    cache_set_headers = (CacheSetHeader *)malloc(num_sets * sizeof(CacheSetHeader));
+    if (cache_set_headers == NULL)
+    {
+        printf("Out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < num_sets; ++i)
+    {
+        // Set to default value
+        cache_set_headers[i].head = NULL;
+        cache_set_headers[i].length = 0;
+    }
+}
+
+void parse_options(int argc, char *argv[])
 {
     int ch = EOF;
     while (~(ch = getopt(argc, argv, optstring)))
@@ -177,7 +206,10 @@ int main(int argc, char *argv[])
             break;
         }
     }
+}
 
+void load_options()
+{
     // Unknown options
     if (invalid_flag)
     {
@@ -185,14 +217,14 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // show help information
+    // Show help information
     if (help_flag)
     {
         printf("Show help information\n");
         exit(EXIT_SUCCESS);
     }
 
-    // numbers must be greater than zero
+    // Numbers must be greater than zero
     if ((set_index_bits <= 0) || (lines_per_set <= 0) || (block_offset_bits <= 0))
     {
         printf("Number error\n");
@@ -200,27 +232,10 @@ int main(int argc, char *argv[])
     }
     else
     {
-        // generate masks
-        block_mask = (1ull << block_offset_bits) - 1;
-        set_mask = ((1ull << set_index_bits) - 1) << block_offset_bits;
-        tag_mask = (ULLONG_MAX ^ set_mask) ^ block_mask;
-        num_sets = 1u << set_index_bits;
-
-        // allocate spaces for cache
-        cache_set_headers = (CacheSetHeader *)malloc(num_sets * sizeof(CacheSetHeader));
-        if (cache_set_headers == NULL)
-        {
-            printf("Out of memory\n");
-            exit(EXIT_FAILURE);
-        }
-        for (int i = 0; i < num_sets; ++i)
-        {
-            cache_set_headers[i].head = NULL;
-            cache_set_headers[i].length = 0;
-        }
+        initialize_cache();
     }
 
-    // file must exist
+    // File must exist
     if (trace_file == NULL)
     {
         printf("File does not exist\n");
@@ -229,21 +244,14 @@ int main(int argc, char *argv[])
     else
     {
         printf("File found: %s\n", trace_file_path);
-        while (~(fscanf(trace_file, "%s %llx,%hd \n", trace_entries[entries_count].operation, &trace_entries[entries_count].address, &trace_entries[entries_count].size)))
-        {
-            unsigned long long current_address = trace_entries[entries_count].address;
-            trace_entries[entries_count].index.tag_index = (tag_mask & current_address) >> (set_index_bits + block_offset_bits);
-            trace_entries[entries_count].index.set_index = (set_mask & current_address) >> block_offset_bits;
-            trace_entries[entries_count].index.block_index = block_mask & current_address;
-            trace_entries[entries_count].result_count = 0;
-            for (int i = 0; i < 4; ++i)
-            {
-                trace_entries[entries_count].operation_results[i] = '\0';
-            }
-            ++entries_count;
-        }
-        fclose(trace_file);
+        read_traces();
     }
+}
+
+int main(int argc, char *argv[])
+{
+    parse_options(argc, argv);
+    load_options();
 
     for (int i = 0; i < entries_count; ++i)
     {
